8e3185df8d9927e19c822a6c37ece20156fd80bc
Dominique MICHEL <dominique.michel@epita.fr>
2022-11-23 00:13:53 +0100

    project: update gitignore o ignore database file

--- flexomserver/__init__.py
+++ flexomserver/__init__.py
@@ -1,6 +1,8 @@
 from .config import FlexomServerConfig
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from pywebpush import webpush
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -8,12 +10,28 @@ app.config['SECRET_KEY'] = FlexomServerConfig.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{FlexomServerConfig.SQLITE_PATH}"
 db.init_app(app)
 
-
 from .models import *
-
-
 with app.app_context():
     db.create_all()
+
+
+vapid_dir = FlexomServerConfig.VAPID_DIR
+with open(os.path.join(vapid_dir, "private_key.pem"), "r+") as f:
+    vapid_private_key = f.readline().strip("\n")
+with open(os.path.join(vapid_dir, "public_key.pem"), "r+") as f:
+    vapid_public_key = f.read().strip("\n")
+vapid_claims = {
+    "sub": FlexomServerConfig.VAPID_CLAIM_EMAIL
+}
+
+
+def send_web_push(subscription_information, message_body):
+    return webpush(
+        subscription_info=subscription_information,
+        data=message_body,
+        vapid_private_key=vapid_private_key,
+        vapid_claims=vapid_claims
+    )
 
 
 from .views import *
--- flexomserver/views.py
+++ flexomserver/views.py
@@ -1,6 +1,53 @@
-from . import app
+import json
+from flask import request, Response, jsonify
+from . import app, vapid_public_key, send_web_push
 
 
-@app.get("/")
-def root():
+@app.route('/')
+def index():
+    # sub = Subscriptions(
+    #     url="https://lol.fr",
+    #     token="lolMDRxd"
+    # )
+    # db.session.add(sub)
+    # db.session.commit()
+    # subs = db.session.execute(db.select(Subscriptions).order_by(Subscriptions.id)).scalars()
+    # subs = list(subs)
     return "Hello World !"
+
+
+@app.route("/subscription/", methods=["GET", "POST"])
+def subscription():
+    """
+        POST creates a subscription
+        GET returns vapid public key which clients use to send around push notification
+    """
+    if request.method == "GET":
+        return Response(
+            response=json.dumps({"public_key": vapid_public_key}),
+            headers={"Access-Control-Allow-Origin": "*"},
+            content_type="application/json"
+        )
+    subscription_token = request.get_json().get("subscription_token")
+    print(subscription_token)
+    return Response(status=201, mimetype="application/json")
+
+
+@app.route("/push_v1/", methods=['POST'])
+def push_v1():
+    message = "Push Test v1"
+    print("is_json", request.is_json)
+
+    if not request.json or not request.json.get('sub_token'):
+        return jsonify({'failed': 1})
+
+    print("request.json", request.json)
+
+    token = request.json.get('sub_token')
+    try:
+        token = json.loads(token)
+        send_web_push(token, message)
+        return jsonify({'success': 1})
+    except Exception as e:
+        print("error", e)
+        return jsonify({'failed': str(e)})
